f6665eab0d8a5663458b8d42a1da5edc86854252
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2022-09-27 23:32:09 +0900

    Init gem

---
+++ lib/mille_feuille.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mille_feuille/version"
+
+module MilleFeuille
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/mille_feuille_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MilleFeuille do
+  it "has a version number" do
+    expect(MilleFeuille::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
